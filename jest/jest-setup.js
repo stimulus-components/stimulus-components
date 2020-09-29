87c1df54443bae1a366af237e234ea4b71c7441e
Guillaume Briday <guillaumebriday@gmail.com>
2020-09-30 00:41:16 +0200

    Adding test for Toggle

--- jest/jest-setup.js
+++ jest/jest-setup.js
@@ -4,7 +4,7 @@ const { setup: setupPuppeteer } = require('jest-environment-puppeteer')
 module.exports = async function globalSetup (globalConfig) {
   await setupPuppeteer(globalConfig)
   await setupDevServer({
-    command: `npx http-server . -p 3000`,
+    command: `yarn dev --open none --port 3000`,
     launchTimeout: 50000,
     port: 3000
   })
