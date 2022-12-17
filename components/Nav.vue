abeb6c290946445407de5e5b8f05bb08b5b54180
Guillaume Briday <guillaumebriday@gmail.com>
2022-12-16 19:57:24 -0500

    Make Navbar sticky

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-white shadow">
+  <nav class="bg-white shadow sticky top-0 z-50">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex justify-between h-16">
         <div class="flex w-full">
