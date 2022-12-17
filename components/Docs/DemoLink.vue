c42a4b0156f97421311a80ee833b57424b19e707
Guillaume Briday <guillaumebriday@gmail.com>
2022-12-16 19:54:03 -0500

    Using button style for demo

--- components/Docs/DemoLink.vue
+++ components/Docs/DemoLink.vue
@@ -2,7 +2,13 @@
   <div>
     <h2>👀 Demo</h2>
 
-    <a :href="`https://stimulus-${packageName}.stimulus-components.com`" target="_blank"> See demo </a>
+    <a
+      :href="`https://stimulus-${packageName}.stimulus-components.com`"
+      target="_blank"
+      class="hover:text-white no-underline rounded-md text-white py-3 px-8 bg-gradient-to-r from-yellow-500 to-orange-500"
+    >
+      See demo &rarr;
+    </a>
   </div>
 </template>
 
