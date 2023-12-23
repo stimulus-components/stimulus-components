0d377589c471cf7b375a3a54b981742d635da467
Guillaume Briday <guillaumebriday@gmail.com>
2023-12-23 12:44:09 -0500

    Updating footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -12,9 +12,11 @@
       <div class="mt-8 flex justify-center space-x-6">
         <a href="https://twitter.com/guillaumebriday" class="text-gray-400 hover:text-gray-500">
           <span class="sr-only">Twitter</span>
-          <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+          <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 512 512">
             <path
-              d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+              fill-rule="evenodd"
+              d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+              clip-rule="evenodd"
             />
           </svg>
         </a>
@@ -33,7 +35,7 @@
 
       <p class="mt-8 text-center text-base text-gray-400">
         &copy; {{ new Date().getFullYear() }} Stimulus Components. Brought to you by
-        <a href="https://guillaumebriday.fr">Guillaume Briday</a>.
+        <a href="https://guillaumebriday.fr" class="hover:underline">Guillaume Briday</a>.
       </p>
     </div>
   </footer>
