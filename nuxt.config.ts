5271ff6c327afcfd0561e50da508063c82f6743d
Guillaume Briday <guillaume.briday@per-angusta.com>
2024-01-09 14:47:38 -0500

    Adding plausible

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@nuxtjs/robots', '@nuxtjs/algolia'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@nuxtjs/robots', '@nuxtjs/algolia', '@nuxtjs/plausible'],
 
   content: {
     markdown: {
