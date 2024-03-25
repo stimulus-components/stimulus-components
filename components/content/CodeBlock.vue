6aa2872824309578a90c24c380815ac357ee90e4
Guillaume Briday <guillaume.briday@per-angusta.com>
2024-03-25 13:48:24 -0400

    Fixing tabs buttons

--- components/content/CodeBlock.vue
+++ components/content/CodeBlock.vue
@@ -4,19 +4,20 @@
       class="bg-zinc-100 dark:bg-slate-800 flex gap-2 items-center justify-between border border-gray-200 dark:border-slate-500 border-b-0 overflow-x-scroll rounded-t-md"
     >
       <div class="flex">
-        <button
+        <component
+          :is="tabs.length === 1 ? 'span' : 'button'"
           v-for="(tab, index) in tabs"
           :key="index"
           tabindex="-1"
           class="flex items-center flex-shrink-0 font-semibold gap-2 text-gray-700 dark:text-white border-b-2 text-sm/6 px-3 py-2 whitespace-nowrap overflow-x-scroll"
           :class="[index === selectedIndex ? 'bg-white dark:bg-slate-700 border-orange-500' : 'border-transparent']"
           type="button"
-          @click="selectedIndex = index"
+          @click.prevent="selectedIndex = index"
         >
           <component :is="tab.icon" class="size-4" />
 
           {{ tab.label }}
-        </button>
+        </component>
       </div>
 
       <CopyToClipboard v-if="props.copyToClipboard" :content="selectedTab?.clipboardContent" />
