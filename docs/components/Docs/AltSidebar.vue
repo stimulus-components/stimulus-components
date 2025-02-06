2ad792cdcbad512e8dd2aaab7ed5f520ab38a2fd
divagueame <martin.arce.teixeira@gmail.com>
2025-02-06 14:25:32 +0100

    adjust layout

--- docs/components/Docs/AltSidebar.vue
+++ docs/components/Docs/AltSidebar.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="hidden lg:block">
-    <nav class="whitespace-nowrap">
-      <h3 class="font-display text-sm font-medium text-slate-900 dark:text-white">Community</h3>
+  <nav class="whitespace-nowrap">
+    <h3 class="font-display text-sm font-medium text-slate-900 dark:text-white">Community</h3>
 
-      <ol role="list" class="mt-2 space-y-3 text-slate-500">
-        <li v-for="link in links" :key="link.title">
-          <a class="hover:text-orange-600 flex gap-2 items-center" :href="link.path" target="_blank">
-            <component :is="link.icon" class="size-5" />
-            {{ link.title }}
-          </a>
-        </li>
-      </ol>
-    </nav>
-  </div>
+    <ol role="list" class="mt-2 space-y-3 text-slate-500">
+      <li v-for="link in links" :key="link.title">
+        <a class="hover:text-orange-600 flex gap-2 items-center" :href="link.path" target="_blank">
+          <component :is="link.icon" class="size-5" />
+          {{ link.title }}
+        </a>
+      </li>
+    </ol>
+  </nav>
 </template>
 
 <script setup>
--- docs/components/Docs/Sidebar.vue
+++ docs/components/Docs/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav>
-    <ul class="space-y-9 whitespace-nowrap">
+  <nav class="block md:sticky md:top-16">
+    <ul class="max-h-[calc(100dvh-64px)] md:overflow-y-scroll space-y-9 whitespace-nowrap md:px-6 md:pt-4 md:pb-8">
       <li v-for="section in sections" :key="section.title">
         <h2 class="dark:text-white font-display font-medium text-slate-900">
           {{ section.title }}
