120327739c571c961d1f352681dd4bbf809d2b46
victor <victor@elalemanyo.de>
2025-02-28 18:08:23 +0100

    Add nextFrame utility function for animation frame handling
    Co-authored-by: Marco Roth <marco.roth@intergga.ch>

--- utils/index.ts
+++ utils/index.ts
@@ -30,3 +30,7 @@ export function throttle(callback: Function, delay: number) {
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
+
+export function nextFrame() {
+  return new Promise((resolve) => requestAnimationFrame(resolve))
+}
